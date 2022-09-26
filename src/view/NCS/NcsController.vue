c3f625c61dcc595b491fefd3fd1268d9a204afe7
jin-hee Park <sonjinh9862@gmail.com>
2022-09-26 17:57:34 +0900

    feat: NCS forder

---
+++ src/view/NCS/NcsController.vue
@@ -0,0 +1,24 @@
+<template>
+  <TopNavDefault />
+  <NCSContainer>
+    <router-view class="view NCSed-content"> </router-view>
+  </NCSContainer>
+</template>
+
+<script>
+import TopNavDefault from "@/components/TopNav/TopNavDefault";
+import styled from "vue3-styled-components";
+
+const NCSContainer = styled("div")`
+  display: flex;
+`;
+
+export default {
+  name: "Controller",
+  components: { NCSContainer, TopNavDefault },
+  props: {},
+  setup(props) {
+    return { ...props };
+  },
+};
+</script>
---
+++ src/view/NCS/NcsedMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <Container> </Container>
+</template>
+
+<script>
+import styled from "vue3-styled-components";
+
+const Container = styled("div")`
+  display: flex;
+`;
+
+export default {
+  name: "NcsedMap",
+  components: {
+    Container,
+  },
+  props: {},
+  setup(props) {
+    return { ...props };
+  },
+};
+</script>
