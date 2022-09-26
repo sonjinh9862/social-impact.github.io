b98f48b6a0b54bb55187c5c441225ce13dd71035
jin-hee Park <sonjinh9862@gmail.com>
2022-09-26 17:55:23 +0900

    환경 정리

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/"),
+      },
+    },
+  },
+  publicPath: process.env.BASE_URL,
+  assetsDir: process.env.BASE_URL,
+  devServer: {
+    contentBase: path.join(__dirname, ""),
+  },
+};
