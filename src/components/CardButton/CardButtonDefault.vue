a954375fd004a5604fa6c423d81acea84767eea8
jin-hee Park <sonjinh9862@gmail.com>
2022-09-26 17:57:56 +0900

    feat: cards

---
+++ src/components/CardButton/CardButtonDefault.vue
@@ -0,0 +1,113 @@
+<template>
+  <Container
+    v-bind:srcUrl="src ? require(`@/assets/${src}.png`) : ''"
+    :isSrc="src"
+  >
+    <BlackBgColor v-bind:color="titleBackground">
+      <Title v-if="title">
+        {{ title }}
+        <Badge v-if="badge">
+          {{ badge }}
+        </Badge>
+      </Title>
+      <Explain v-if="explain">
+        {{ explain }}
+      </Explain>
+    </BlackBgColor>
+  </Container>
+</template>
+
+<script>
+import styled from "vue3-styled-components";
+
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  border-radius: 30px;
+  height: 205px;
+  background-image: url(${(props) => (props.srcUrl ? `${props.srcUrl}` : "")});
+  background-repeat: none;
+  background-size: cover;
+  background-position: center;
+  background-color: ${(props) => (props.isSrc ? "" : "#D9D9D9")};
+  box-sizing: border-box;
+  overflow: hidden;
+  filter: drop-shadow(0 4px 3px rgba(0, 0, 0, 0.3));
+  box-shadow: inset 0px 4px 8px rgba(0, 0, 0, 0.3);
+`;
+
+const Explain = styled("div")`
+  font-size: 3px;
+  color: #ffffff;
+  text-align: center;
+  font-weight: 600;
+  text-align: start;
+  width: 100%;
+`;
+
+const Title = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  line-height: 1.2;
+  font-size: 18px;
+  font-weight: 700;
+  color: #ffffff;
+  text-align: left;
+  white-space: pre;
+`;
+
+const Badge = styled("div")`
+  position: absolute;
+  margin-left: 60px;
+  transform: rotate(40deg);
+  font-size: 10px;
+  font-weight: 600;
+  color: #ffffff;
+`;
+
+const BlackBgColor = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 7px 17px 0 17px;
+  width: 100%;
+  height: 84px;
+  background-color: ${(props) => (props.color ? `rgba(0, 0, 0, 0.4)` : "")};
+`;
+
+export default {
+  name: "CardButtonDefault",
+  components: { Container, Explain, Title, Badge, BlackBgColor },
+  props: {
+    src: {
+      type: String,
+      default: "",
+    },
+    explain: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    badge: {
+      type: String,
+      default: "",
+    },
+    titleBackground: {
+      type: Boolean,
+      default: undefined,
+    },
+  },
+  setup(props) {
+    const imgUrl = "src/assets/" + props.src + ".png";
+    return { ...props, imgUrl };
+  },
+};
+</script>
---
+++ src/components/CardButton/CardButtonLong.vue
@@ -0,0 +1,76 @@
+<template>
+  <Container v-bind:src="src">
+    <Explain v-if="explain">
+      {{ explain }}
+    </Explain>
+    <Title v-if="title" v-bind:isExplain="explain">
+      {{ title }}
+    </Title>
+  </Container>
+</template>
+
+<script>
+import styled from "vue3-styled-components";
+
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 17px 23px;
+  height: 435px;
+  border-radius: 30px;
+  background-image: url(${(props) =>
+    props.src ? "src/assets/" + props.src + ".png" : ""});
+  background-repeat: none;
+  background-size: cover;
+  background-position: center;
+  background-color: ${(props) => (props.src ? "" : "#D9D9D9")};
+  box-sizing: border-box;
+`;
+
+const Explain = styled("div")`
+  font-size: 10px;
+  color: #ffffff;
+  text-align: start;
+  font-weight: 600;
+  width: 100%;
+`;
+
+const Title = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  line-height: 1.5;
+  gap: 7px;
+  margin-top: ${(props) => (props.isExplain ? "30px" : "41px")};
+  font-size: 25px;
+  font-weight: 900;
+  color: #ffffff;
+  text-align: left;
+  white-space: pre;
+`;
+
+export default {
+  name: "CardButtonLong",
+  components: { Container, Explain, Title },
+  props: {
+    src: {
+      type: String,
+      default: "",
+    },
+    explain: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props) {
+    return { ...props };
+  },
+};
+</script>
