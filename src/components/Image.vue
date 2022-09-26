81741709f6ca00553cfc3665c4337fecb4544bc3
jin-hee Park <sonjinh9862@gmail.com>
2022-09-26 17:57:47 +0900

    feat: image component

---
+++ src/components/Image.vue
@@ -0,0 +1,23 @@
+<template>
+  <StyledImg :src="require(`@/assets/${src}.png`)" :width="width" />
+</template>
+
+<script>
+import styled from "vue3-styled-components";
+
+const StyledImg = styled("img")`
+  width: ${(props) => (props.width ? props.width : "5px")};
+`;
+
+export default {
+  name: "Image",
+  components: { StyledImg },
+  props: {
+    src: String,
+    width: Number,
+  },
+  setup(props) {
+    return { ...props };
+  },
+};
+</script>
